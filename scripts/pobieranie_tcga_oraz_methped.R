9dcbc2bb08ccfa2cd40f6100211d6a226c1edc0c
WeronikaPlichta375 <103688816+WeronikaPlichta375@users.noreply.github.com>
2023-03-19 12:37:42 +0100

    Small improvments
    - Adding constants for easier customization
    - Adding platform constant (as we've tested that samples from illumina human methylation 450,
    there is lower number of missing probes)

--- scripts/pobieranie_tcga_oraz_methped.R
+++ scripts/pobieranie_tcga_oraz_methped.R
@@ -1,24 +1,37 @@
 # skrypt do pobierania danych z tcga pakietem GenomicDataCommons i przekazywania do MethPed 
 # na razie bez filtrowania czy pediatryczne czy nie, tylko sprawdzenie czy działa
 
-# połączenie API - powinno zwracać status OK
+# Constants (values to customise):
+AGE <- 18
+NR_OF_TOTAL_SAMPLES_TO_DOWNLOAD<- 3
+PROJECT_ID <- 'TCGA-GBM'
+PLATFORM <- "illumina human methylation 450"
+
 library(GenomicDataCommons)
+library(MethPed)
+# połączenie API - powinno zwracać status OK
 GenomicDataCommons::status()
 
 # szukanie plików z konkretnego projektu tutaj akurat 
 # https://www.cancer.gov/ccg/research/genome-sequencing/tcga/studied-cancers/glioblastoma-multiforme-study
 # zawierających dane o metylacji
 ge_manifest <- files() |>
-  filter( cases.project.project_id == 'TCGA-GBM') |>
+  filter( cases.project.project_id == PROJECT_ID) |>
   filter( type == 'methylation_beta_value' ) |>
-  filter(cases.demographic.days_to_birth < -60*365) |> # filtrowanie wieku PAMIĘTAĆ O MINUSIE
+  filter(cases.demographic.days_to_birth < -AGE*365) |> # filtrowanie wieku PAMIĘTAĆ O MINUSIE
+  filter(platform == PLATFORM) |>
   manifest()
 ge_manifest
 # ge_manifest zawiera informacje o plikach ale jeszcze nie dane
 
 # dla każdego id pliku z ge_manifest ściągamy dane funckją gdcdata
 # za pierwszym razem może zapytać czy stworzyć folder, odpowiadamy że tak
-fnames = lapply(ge_manifest$id, gdcdata) # ogólnie lepiej nie ściągać od razu wszystkiego
+
+#To dowload all the file (do not recommend if you don't know the size)
+#fnames = lapply(ge_manifest$id, gdcdata) # ogólnie lepiej nie ściągać od razu wszystkiego
+
+#To download desired amount of samples
+fnames = lapply(ge_manifest$id[1:NR_OF_TOTAL_SAMPLES_TO_DOWNLOAD], gdcdata)
 # bo jest bardzo dużo tutaj można zaindeksować np. ge_manifest$id[1:5]
 # albo wyżej przy tworzeniu ge_manifest dać więcej filtrów
 # fnames to named list ze ścieżkami pobranych plików .txt
@@ -35,11 +48,11 @@ data = data.frame(TargetID = character(), V2 = numeric(), id = character())
 i = 1
 for(x in fnames){
   print(i)
-  i = i+1
   temp_data = read.delim(x[[1]], header = FALSE)
   temp_data = dplyr::rename(temp_data, c('TargetID' = 'V1'))
   temp_data$id = rep(names(fnames[[i]]), nrow(temp_data))
   data = dplyr::bind_rows(data, temp_data)
+  i = i+1
 }
 data = tidyr::pivot_wider(data, names_from = id, values_from = V2)
 
